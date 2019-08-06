303ffe1869694ff00c1b2b3d0b3ab5a315fe4cf2
Krasen <krasenvachev3@gmail.com>
2019-08-06 23:37:06 +0300

    top rated courses now gets data from rest

--- src/components/NotificationComponent/NotificationComponent.js
+++ src/components/NotificationComponent/NotificationComponent.js
@@ -1,74 +1,75 @@
-import React from "react"
-import { MDBDropdown, MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem } from "mdbreact";
-import "./NotificationComponent.css"
-import axios from "axios"
+import React from "react";
+import {
+  MDBDropdown,
+  MDBDropdownToggle,
+  MDBDropdownMenu,
+  MDBDropdownItem
+} from "mdbreact";
+import "./NotificationComponent.css";
+import axios from "axios";
 
 class NotificationComponent extends React.Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props)
+    this.state = {
+      notifications: [],
+      defaultColor: "light-blue",
+      warningColor: "amber"
+    };
+  }
 
-        this.state = {
-            notifications: [],
-            defaultColor: "light-blue",
-            warningColor: "amber"
-        }
+  clickHandler = event => {
+    event.preventDefault();
+  };
+
+  updateNotifications = () => {
+    let newNotifications = [];
+
+    axios
+      .get(
+        "http://localhost:8080/api/notifications/unseen/" + this.props.userId
+      )
+      .then(response => {
+        console.log(response);
+      });
+  };
+
+  componentDidMount() {
+    this.updateNotifications();
+  }
+
+  render() {
+    let color =
+      this.state.notifications.length === 0
+        ? this.state.defaultColor
+        : this.state.warningColor;
+
+    let notificationsText;
+    switch (this.state.notifications.length) {
+      case 0:
+        notificationsText = "No Notifications";
+        break;
+      case 1:
+        notificationsText = "(1) NEW Notification";
+        break;
+      default:
+        notificationsText =
+          "(" + this.state.notifications.length + ") NEW Notifications!";
     }
 
-    clickHandler = event => {
-        event.preventDefault();
-    }
-
-    updateNotifications = () => {
-
-        let newNotifications = [];
-
-        axios
-        .get("http://localhost:8080/api/notifications/unseen/"+this.props.userId)
-        .then(response => {
-          console.log(response);
-        });
-    }
-
-    componentDidMount() {
-        this.updateNotifications();
-    }
-
-
-    render(){
-
-        let color = this.state.notifications.length === 0 ?
-            this.state.defaultColor :
-            this.state.warningColor;
-
-        let notificationsText;
-        switch (this.state.notifications.length) {
-            case 0:
-                notificationsText = "No Notifications";
-                break;
-            case 1:
-                notificationsText = "(1) NEW Notification";
-                break;
-            default:
-                notificationsText = "("+this.state.notifications.length+") NEW Notifications!";
-        }
-
-        return(
-            <MDBDropdown dropleft>
-            <MDBDropdownToggle
-            color={color}
-            size="sm"
-            >
-              {notificationsText}
-            </MDBDropdownToggle>
-            <MDBDropdownMenu basic>
-              <MDBDropdownItem>Action</MDBDropdownItem>
-              <MDBDropdownItem>Another Action</MDBDropdownItem>
-            </MDBDropdownMenu>
-          </MDBDropdown>
-        );
-    }
-
+    return (
+      <MDBDropdown dropleft>
+        <MDBDropdownToggle color={color} size="sm">
+          {notificationsText}
+        </MDBDropdownToggle>
+        <MDBDropdownMenu basic>
+          <MDBDropdownItem>Action</MDBDropdownItem>
+          <MDBDropdownItem>Another Action</MDBDropdownItem>
+        </MDBDropdownMenu>
+      </MDBDropdown>
+    );
+  }
 }
 
 export default NotificationComponent;
--- src/containers/CoursesContainer/CoursesContainer.js
+++ src/containers/CoursesContainer/CoursesContainer.js
@@ -1,50 +1,55 @@
 import React, { Component } from "react";
-import './CoursesContainer.css';
+import "./CoursesContainer.css";
 import CourseCardsContainer from "../CourseCardsContainer/CourseCardsContainer";
-import { MDBContainer } from 'mdbreact'
+import { MDBContainer } from "mdbreact";
 
 class CoursesContainer extends Component {
-    render() {
-        return (
-            <div className="CoursesContainer">
-                <MDBContainer className="mx-auto my-1">
-                    <h3>Top courses in Topic 1</h3>
-                    <br></br>
-                    <CourseCardsContainer></CourseCardsContainer>
-                </MDBContainer><MDBContainer className="mx-auto my-1">
-                    <br></br>
-                    <hr></hr>
-                    <h3>Top courses in Topic 2</h3>
-                    <br></br>
-                    <CourseCardsContainer></CourseCardsContainer>
-                </MDBContainer><MDBContainer className="mx-auto my-1">
-                    <br></br>
-                    <hr></hr>
-                    <h3>Top courses in Topic 3</h3>
-                    <br></br>
-                    <CourseCardsContainer></CourseCardsContainer>
-                </MDBContainer><MDBContainer className="mx-auto my-1">
-                    <br></br>
-                    <hr></hr>
-                    <h3>Top courses in Topic 4</h3>
-                    <br></br>
-                    <CourseCardsContainer></CourseCardsContainer>
-                </MDBContainer><MDBContainer className="mx-auto my-1">
-                    <br></br>
-                    <hr></hr>
-                    <h3>Top courses in Topic 5</h3>
-                    <br></br>
-                    <CourseCardsContainer></CourseCardsContainer>
-                </MDBContainer><MDBContainer className="mx-auto my-1">
-                    <br></br>
-                    <hr></hr>
-                    <h3>Top courses in Topic 6</h3>
-                    <br></br>
-                    <CourseCardsContainer></CourseCardsContainer>
-                </MDBContainer>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="CoursesContainer">
+        <MDBContainer className="mx-auto my-1">
+          <h3>Top courses in Topic 1</h3>
+          <br />
+          <CourseCardsContainer />
+        </MDBContainer>
+        <MDBContainer className="mx-auto my-1">
+          <br />
+          <hr />
+          <h3>Top courses in Topic 2</h3>
+          <br />
+          <CourseCardsContainer />
+        </MDBContainer>
+        <MDBContainer className="mx-auto my-1">
+          <br />
+          <hr />
+          <h3>Top courses in Topic 3</h3>
+          <br />
+          <CourseCardsContainer />
+        </MDBContainer>
+        <MDBContainer className="mx-auto my-1">
+          <br />
+          <hr />
+          <h3>Top courses in Topic 4</h3>
+          <br />
+          <CourseCardsContainer />
+        </MDBContainer>
+        <MDBContainer className="mx-auto my-1">
+          <br />
+          <hr />
+          <h3>Top courses in Topic 5</h3>
+          <br />
+          <CourseCardsContainer />
+        </MDBContainer>
+        <MDBContainer className="mx-auto my-1">
+          <br />
+          <hr />
+          <h3>Top courses in Topic 6</h3>
+          <br />
+          <CourseCardsContainer />
+        </MDBContainer>
+      </div>
+    );
+  }
 }
 
 export default CoursesContainer;
