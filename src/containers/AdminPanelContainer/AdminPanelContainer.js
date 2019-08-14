3d018e4f61e8a6496c491d5f278666138644fd10
Krasen <krasenvachev3@gmail.com>
2019-08-15 02:17:01 +0300

    fixed admin panel access control

--- src/containers/AdminPanelContainer/AdminPanelContainer.js
+++ src/containers/AdminPanelContainer/AdminPanelContainer.js
@@ -1,94 +1,73 @@
-import React from "react"
-import { Redirect } from "react-router-dom"
+import React from "react";
+import { Redirect } from "react-router-dom";
 import { MDBContainer, MDBCol, MDBRow, MDBBtn, MDBBtnGroup } from "mdbreact";
 import TeacherRequestsComponent from "../../components/TeacherRequestsComponent/TeacherRequestsComponent";
 import UserManagerComponent from "../../components/UserManagerComponent/UserManagerComponent";
-import axios from "axios"
-import "./AdminPanelContainer.css"
+import "./AdminPanelContainer.css";
 
 class AdminPanelContainer extends React.Component {
-    constructor(props) {
-        super(props);
-    
-        this.state = {
-          teacherRequests: [],
-          users:[],
-          rightSideOption: 0,
-          profile: null
-        };
-      }
+  constructor(props) {
+    super(props);
 
-      componentWillMount() {
-        let config = {
-          headers: {
-            Authorization: "Bearer " + this.props.authInfo.authToken
-          }
-        };
-    
-        axios
-          .get(`http://localhost:8080/api/users/me`, config)
-          .then(response => {
-            this.setState({
-              profile: response.data
-            });
-          })
-          .catch(error => {
-            console.log(error.response);
-          });
-      }
+    this.state = {
+      teacherRequests: [],
+      users: [],
+      rightSideOption: 0,
+      profile: null
+    };
+  }
 
-      onClick = num => () => {
-        this.setState({
-          rightSideOption: num
-        });
-      };
+  onClick = num => () => {
+    this.setState({
+      rightSideOption: num
+    });
+  };
 
-      render() {
-        
-        if (this.state.profile !== null && this.state.profile.roles.length < 3) {
-          return (<Redirect to="/404" />)
-        }
+  render() {
+    if (this.props.authInfo.Admin !== true) {
+      return <Redirect to="/404" />;
+    }
 
-        let rightSide = null;
+    let rightSide = null;
 
-        switch (this.state.rightSideOption) {
-          case 0:
-            rightSide = (
-              <TeacherRequestsComponent
-                profile={this.state.profile}
-                authInfo={this.props.authInfo}
-                authHandler={this.props.authHandler}
-                logoutHandler={this.props.logoutHandler}
-              />
-            );
-            break;
-          case 1:
-              rightSide = (
-                <UserManagerComponent
-                  profile={this.state.profile}
-                  authInfo={this.props.authInfo}
-                  authHandler={this.props.authHandler}
-                  logoutHandler={this.props.logoutHandler}
-                />
-              );
-            break;
-          default:
-            rightSide = (
-              <TeacherRequestsComponent
-                profile={this.state.profile}
-                authInfo={this.props.authInfo}
-                authHandler={this.props.authHandler}
-                logoutHandler={this.props.logoutHandler}
-              />
-            );
-        }
+    switch (this.state.rightSideOption) {
+      case 0:
+        rightSide = (
+          <TeacherRequestsComponent
+            profile={this.state.profile}
+            authInfo={this.props.authInfo}
+            authHandler={this.props.authHandler}
+            logoutHandler={this.props.logoutHandler}
+          />
+        );
+        break;
+      case 1:
+        rightSide = (
+          <UserManagerComponent
+            profile={this.state.profile}
+            authInfo={this.props.authInfo}
+            authHandler={this.props.authHandler}
+            logoutHandler={this.props.logoutHandler}
+          />
+        );
+        break;
+      default:
+        rightSide = (
+          <TeacherRequestsComponent
+            profile={this.state.profile}
+            authInfo={this.props.authInfo}
+            authHandler={this.props.authHandler}
+            logoutHandler={this.props.logoutHandler}
+          />
+        );
+    }
 
-        return(      
-        <div className="AdminPanelContainer">
+    return (
+      <div className="AdminPanelContainer">
         <MDBContainer>
           <MDBRow>
             <MDBCol lg="2">
-              <h3>Manage:   </h3>
+              <h3>Manage: </h3>
               <hr />
               <MDBBtnGroup vertical>
                 <MDBBtn
@@ -110,8 +89,9 @@ class AdminPanelContainer extends React.Component {
             <MDBCol lg="10">{rightSide}</MDBCol>
           </MDBRow>
         </MDBContainer>
-      </div>);
-      }
+      </div>
+    );
+  }
 }
 
 export default AdminPanelContainer;
